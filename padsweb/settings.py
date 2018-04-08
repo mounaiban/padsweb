e24bef3b9282f8d7fd054815764b9da7367cb15b
mounaiban <29518031+mounaiban@users.noreply.github.com>
2018-04-09 08:47:41 +1000

    Add password settings to dictionary, update version numbers

--- padsweb/settings.py
+++ padsweb/settings.py
@@ -11,13 +11,21 @@ from padsweb.strings import PADSStringDictionary
 # Hard-coded configuration in dictionary
 #
 app_metadata = {
+        # Please do not alter app metadata during normal operation
         'copyright_message_html' : '&copy; 2018 Mounaiban',
-        'current_version' : '0.590',
+        'current_version' : '0.588-pre590',
         }
 
 defaults_content = {
-        'view_items_per_page' : 6,
+        # The letter 'O' and number '1' are omitted
+        'ql_password_letters' : 'ABCDEFGHIJKLMNPQRSTUVWXYZ234567890',
+        'ql_password_length' : 8,
+        'ql_password_max_segments' : 3,
+        'ql_password_seg_separator' : '-',
+        'ql_user_name_suffix_length' : 12,
+        'password_salt_bytes' : 48,
         'user_id_signed_out' : -1, # User id when no User has signed in
+        'view_items_per_page' : 6,
         }
 defaults = PADSStringDictionary(defaults_content,
                                 "PADS Hard-coded Default Settings")
@@ -28,7 +36,7 @@ defaults = PADSStringDictionary(defaults_content,
 #
 
 # App Metadata
-PADS_CURRENT_VERSION = '0.587'
+PADS_CURRENT_VERSION = '0.588'
 
 # App Miscellaneous Settings
 MAX_NAME_LENGTH_SHORT = 24
